f6b649cc4aea1a1160345c92fb7e1453df0cf150
yukonblue <77284557+yukonblue@users.noreply.github.com>
2025-12-21 09:34:05 -0500

    Introduce Workspace and LocalizedStringMacrosTestApp

---
+++ LocalizedStringMacrosTestApp/LocalizedStringMacrosTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizedStringMacrosTestApp
+//
+//  Created by Y on 2025-12-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
